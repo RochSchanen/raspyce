683b63bb610452dfb2f482c7b38c86402bcb9f66
Roch Schanen <r.schanen@lancaster.ac.uk>
2024-01-01 14:17:04 +0000

    save spi v1.00

--- BW_SPI_V1.00.py
+++ BW_SPI_V1.00.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
-# debug flags check
+# absolutely no speed optimisation
+# the coding style is for development
+
+_DEBUG = [
+    "NONE",
+    #"ALL",
+    #"TRANSFER",
+    #"TRACE",
+    #"DELAY",
+]
+
+_DELAY = 1
+
+# debug flags checker
 def _debug(*flags):
     if "NONE" in _DEBUG: return False
     if "ALL"  in _DEBUG: return True
@@ -39,8 +52,8 @@ class spi():
     """
         The clock polarity and data phase depends
         on the slave device mode/configuration.
-        check from the device data sheet.
-        Most commonly used is CPOL = 0 and CPHA = 0.
+        check from the device data sheet. Most
+        commonly used is CPOL = 0 and CPHA = 0.
 
         Also, the data bit width determines how many bit
         are send during one data transfer. for one byte at
@@ -64,11 +77,16 @@ class spi():
 
         from RPi.GPIO import setup, IN, OUT
         from RPi.GPIO import output, HIGH, LOW
+        
         # deactivate chip select (CS0 = 1)
         setup(self.PIN_CS, OUT)
         output(self.PIN_CS, HIGH)
 
         # setup other spi pin directions
+        # (MASTER is the raspberry pi):
+        # MOSI > out
+        # MISO < in
+        # SCK  > out
         setup([self.PIN_SCK, self.PIN_MOSI], OUT)
         setup(self.PIN_MISO, IN)
 
@@ -82,7 +100,7 @@ class spi():
         return
 
     def wait(self, duration):
-        """ duration given in milliseconds """
+        """ wait for a given duration (in milliseconds) """
         from time import sleep
         sleep(duration/1000)
         return
@@ -95,10 +113,15 @@ class spi():
             transfer. The character '1' for level
             high and character '0' for level low.
             A bit is transfered every clock cycle.
-            Each cycle is subdivised in halves.
-            An n bits transfer requires 2n halves.
-            An extra redondant half
-        """
+            Each cycle is subdivised in two halves.
+            Thus, an n bits transfer requires 2n
+            halves. An extra half cycle is present
+            to allow for the chip select signal to
+            fall and rise at the start and at the
+            end of the sequence. data it the value
+            to transfer and is given as an integer
+            number.
+            """
 
         w = self.BITW
         # build select sequence
@@ -106,39 +129,41 @@ class spi():
         # build clock sequences
         CLK_S0 =  f"0{w*'01'}00" # clock levels (CPOL 0)
         CLK_S1 =  f"1{w*'10'}11" # clock levels (CPOL 1)
-        # build formatting string for data output sequence
+        # formatting string for data output sequence
         FS = f"0{w}b"
         # build data output sequences
         DTO = "".join([f"{c}{c}" for c in f"{data:{FS}}"])
-        DTO_S0 =  f"0{DTO}00" # data phase (CPHA 0)
-        DTO_S1 =  f"00{DTO}0" # data phase (CPHA 1)
+        DTO_S0 =  f"0{DTO}00" # data levels (CPHA 0)
+        DTO_S1 =  f"00{DTO}0" # data levels (CPHA 1)
         # build data read trigger sequence
+        # (This defines when to record the MISO pin value)
         TRG = f"{w*'01'}"
-        TRG_S0 =  f"0{TRG}00" # data phase (CPHA 0)
-        TRG_S1 =  f"00{TRG}0" # data phase (CPHA 1)
-        # select mode
+        TRG_S0 =  f"0{TRG}00" # record triggers (CPHA 0)
+        TRG_S1 =  f"00{TRG}0" # record triggers (CPHA 1)
+        # select mode (from the values of CPOL and CPHAS)
         CLK_SQ = [CLK_S0, CLK_S1][self.CPOL]
         DTO_SQ = [DTO_S0, DTO_S1][self.CPHA]
         TRG_SQ = [TRG_S0, TRG_S1][self.CPHA]
-        # casting dictionary: characters to GPIO levels
+        # buid casting dictionaries: characters to GPIO levels
         from RPi.GPIO import output as pinwrite
         from RPi.GPIO import input as pinread
         from RPi.GPIO import HIGH, LOW
         # type casting dicos
         LEVEL = {"0": LOW, "1": HIGH}
         SYMBL = {LOW: "0", HIGH: "1"}
-        # declare input list
+        # declare input lists
         DATA, MISO = [], []
         # transfer
         for SEL, CLK, DTO, TRG in zip(SEL_SQ, CLK_SQ, DTO_SQ, TRG_SQ):
+            # process I/O (outputs before inputs)
             pinwrite(self.PIN_MOSI, LEVEL[DTO])
             pinwrite(self.PIN_SCK, LEVEL[CLK])
             pinwrite(self.PIN_CS, LEVEL[SEL])
             MISO.append(SYMBL[pinread(self.PIN_MISO)])
+            # record input value on trigger
             if TRG == "1": DATA.append(MISO[-1])
             # debug
-            if _debug("DELAY"): s.wait(_delay)
-            s.wait(20) # always wait 25ms
+            if _debug("DELAY"): s.wait(_DELAY)
         # debug
         if _debug("TRANSFER"):
             print(f"---------- transfer ----------\n")
@@ -155,15 +180,15 @@ class spi():
             print()
         return int("".join(DATA), 2)
 
-    # display sequence using unicode chars
+    # display sequence using a set of unicode chars
     def display_sequence(self, SQ, NAME):
-        """ using the following uniccode chars
+        """ the following set of unicode chars are used
             u2504 ┄ u2510 ┐ u2534 ┴ u2500 ─
             u2514 └ u2518 ┘ u252C ┬ u250C ┌
         """
         # init sequence at unknown state
         l, T, B = "U", "┄", "┄"
-        # level transition select (top, bottpm) unicode chars
+        # level transition dictionaries (think machine state)
         level_to_level = {
             "U": {"0": ("┐ ", "┴─"), "1": ("┬─", "┘ "), "U": ("┄┄", "┄┄")},
             "0": {"0": ("  ", "──"), "1": ("┌─", "┘ "), },
@@ -171,9 +196,9 @@ class spi():
             }
         # run through sequence
         for c in SQ:
-            # get unicode chars
+            # get unicode pattern (top and bottom chars)
             t, b = level_to_level[l][c]
-            # update state and strings
+            # update state (level value) and strings (top and bottom)
             l, T, B = c, T+t, B+b
         # display
         print(f"{' ':{len(NAME)}}{''.join(T)}")
@@ -181,16 +206,25 @@ class spi():
         # done
         return
 
-# this can be tested using a loop back setup: connect MISO to MOSI
+if __name__ == "__main__":
+    # try using a loop back setup for testing: connect MISO to MOSI
 
-_DEBUG = [
-    #"NONE",
-    #"ALL",
-    "TRANSFER",
-    "TRACE",
-    "DELAY",
-]
-
-s.wait(500)
-from RPi.GPIO import cleanup
-cleanup()
+    _DELAY = 1
+    
+    _DEBUG = [
+        #"NONE",
+        #"ALL",
+        "TRANSFER",
+        "TRACE",
+        #"DELAY",
+        "CLEANUP",
+    ]
+    
+    s = spi()
+    s.transfer(0b00001111)
+    s.transfer(0b11110000)
+    
+    if _debug("CLEANUP"): 
+        s.wait(500)
+        from RPi.GPIO import cleanup
+        cleanup()
